6ee85298a12d467c43426c099d94f9ad6929ba91
noizy-sthlm <57321964+noizy-sthlm@users.noreply.github.com>
2025-09-24 13:32:04 +0200

    express hello world for the tutorial
    - including .env secret

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from "express";
+import 'dotenv/config';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const THE_SECRET = process.env.THE_SECRET || "No_Secret";
+
+app.get("/", (_req, res) => res.json({ok: true, secret: THE_SECRET}));
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));   
