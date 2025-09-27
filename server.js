61960e1c80dbf5ee889210a5d726efa3cafaf920
Fauzan Helmi Sudaryanto <>
2025-09-27 10:11:50 +0200

    add comment secret in server.js

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const THE_SECRET = process.env.THE_SECRET || "No_Secret";
 
+// my secret key is "123456789"
+
 app.get("/", (_req, res) => res.json({ok: true, secret: THE_SECRET}));
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));   
